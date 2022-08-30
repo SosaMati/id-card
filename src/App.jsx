2157ee34b9142dc31ea12af10099753a02981ac2
SosaMati <matiasezequielsosa@gmail.com>
2022-08-30 19:50:33 -0300

    cv corregido

--- src/App.jsx
+++ src/App.jsx
@@ -37,8 +37,8 @@ function App() {
       </div>
 
         <div className='contenedorCv'>
-          <div className='cv'> <a href="https://www.canva.com/design/DAEo4LWsASs/7aF5F49495bUy1PgDnFNQw/view?">VER CV</a> </div>
-          <div className='cv'> <a href="https://matiasezequielsosa.vercel.app/">VER PORTFOLIO</a> </div>
+          <div className='cv'> <a href="https://www.canva.com/design/DAEo4LWsASs/7aF5F49495bUy1PgDnFNQw/view?" target="_blank" rel="noreferrer">VER CV</a> </div>
+          <div className='cv'> <a href="https://matiasezequielsosa.vercel.app/" target="_blank" rel="noreferrer">VER PORTFOLIO</a> </div>
         </div>
 
 
